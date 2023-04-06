bd48bb45a73daf782ae19cac1de51697816f1c44
followthedopamine <83159854+followthedopamine@users.noreply.github.com>
2023-04-07 03:58:36 +1200

    Split up paragraphs and headings

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,18 @@
+import { textToMemorize } from "./text.js";
 const app = document.getElementById("app");
 
-app.innerHTML = "Test";
+const processText = (text) => {
+  let newText = "";
+  text = text.split("\n");
+  for (let i = 0; i < text.length; i++) {
+    if (text[i].startsWith("#")) {
+      text[i] = "<h4>" + text[i] + "</h4>";
+    } else {
+      text[i] = "<p>" + text[i] + "</p>";
+    }
+  }
+  newText = text.join("");
+  return newText;
+};
+
+app.innerHTML = processText(textToMemorize);
---
+++ scripts/text.js
@@ -0,0 +1,106 @@
+const textToMemorize = `## "Hello, thanks for applying for the internship at our company. Can you tell me a little bit about yourself and what interests you about this particular internship?"
+
+My name's Mark. I'm a self taught software developer specializing in web and game development. I've been programming since I was 13 years old, starting with cheating at video games and hosting private servers. Although I haven't had any formal industry experience yet I've been able to develop a strong skillset through personal projects and exploration.
+
+At 17, I completed a diploma in Web Design and Development from a design school and after that I worked as a content creator and video editor for 10 years where I developed my skills by scripting chatbots and automating video editing and the uploading process.
+
+Currently I'm focused on game development to further sharpen my skills. I find it to be a fun and challenging way to keep learning and improving my coding abilities and there are plenty of resources available online to support my learning.
+
+## Great, thank you for sharing that with me, Mark. Can you tell me about a particularly challenging programming project you have worked on and how you overcame any difficulties you encountered?
+
+For me, the most challenging project so far was developing a match 3 style game for Android using Unity. The game's large scope posed several difficulties, especially when it came to incorporating tilemaps and designing for various mobile device dimensions. However, I was able to overcome these obstacles by implementing responsive design techniques and leveraging Unity's powerful features. In the end, the project turned out to be a great success and helped me grow as a developer.
+
+## Hi, thank you for applying for the software development internship at our company. Can you tell me about your experience with programming languages and which ones you are proficient in?
+
+"I am proficient in C#, C++, HTML, CSS, JS, PHP, and Python. I have completed several notable projects, including a fully-featured match 3 game developed in Unity within 48 hours, and a game developed in a team for the Github Game Off. Additionally, I have worked on various front-end projects using VueJS, such as creating simple web applications and interactive user interfaces."
+
+## Great, let's move on to the next question. As an intern at our company, you will be working on a variety of projects. Can you tell me about a project you have worked on in the past and your role in that project?
+
+The most challenging project I've worked on was developing a match 3 style game for Android using Unity entirely on my own. I was responsible for programming the game mechanics, designing the user interface, implementing responsive design techniques, and working with tilemaps to ensure the game worked on a variety of mobile device dimensions. Despite the challenges I faced, I was able to leverage Unity's powerful features to create a successful game that has received positive feedback from users.
+
+## Can you tell me about a time when you had to communicate a complex technical issue to a non-technical stakeholder?
+
+I have no experience in this unfortunately but I think the best way to do it is to break it down into smaller easier to understand concepts and try to empathise with the client as much as I can. Making it clear that they should ask any clarifying questions that they need.
+
+Based on the amount of data we're working with, our optimization algorithm is taking longer than expected to run. This means that the project might be delayed, but ultimately we believe it's more responsible to take the time to fully optimize the algorithm. Think of it like packing for a trip - if you don't take the time to carefully plan what you need, you might end up with a lot of extra baggage that slows you down. We want to make sure that your project runs smoothly and efficiently, so we're taking the time now to optimize the algorithm and avoid any potential delays or added costs in the future.
+
+## Can you tell me about a time when you had to work with a difficult team member or colleague? How did you handle the situation?
+
+As a team member in my current role, I noticed a colleague who was handling delicate mangoes incorrectly, which could lead to customer complaints and unnecessary waste. Although my colleague was initially resistant to my feedback, I took a leadership role by demonstrating empathy and active listening to address his concerns. I agreed with him that the issue was a challenging one, but I explained that it was our responsibility to do everything possible to minimize any negative impacts on the product. By sharing my knowledge and experience with my colleague, I was able to help him understand the importance of handling the mangoes with care.
+
+## Can you describe a time when you had to work collaboratively with a team to solve a complex problem?
+
+While I haven't had direct experience with pair programming, I understand that it involves two programmers working together on the same codebase, with one person coding while the other provides feedback and helps with problem-solving. I can see how this could be beneficial in terms of catching errors early on and improving the overall quality of the code. Additionally, it could promote better collaboration and communication between team members, leading to a more productive and efficient workflow. If given the opportunity, I would be open to exploring pair programming and learning more about it in order to improve my skills as a developer and contribute to the success of the team.
+
+## Can you describe a time when you had to learn a new technology or programming language quickly to complete a project? How did you approach this challenge?
+
+I believe that being able to learn quickly is essential in the fast-paced tech industry. To continuously improve my ability to learn quickly, I actively seek out opportunities to challenge myself and learn new skills. For instance, during the Github game jam, I had to develop a game using Unity within a month, despite never having used the system before. To quickly learn the system, I focused on online tutorials and forums to get up to speed on the basics. Additionally, I leveraged Unity's extensive documentation and community resources to help me overcome specific obstacles. By taking an agile approach and embracing a growth mindset, I was able to learn quickly and develop a successful game.
+
+## How do you prioritize and manage your tasks and projects?
+
+I have found that staying organized and setting clear goals helps me manage my projects effectively. I usually start by creating a comprehensive list of tasks, which I organize by priority and deadline. For smaller projects, I keep the list within the project folder, while for larger ones, I use a project management tool like Trello to track progress and assign tasks to team members if necessary. I also break down larger tasks into smaller subtasks and set achievable deadlines for each to ensure that I'm making progress towards completing the project on time
+
+## Describe your leadership style
+
+My leadership style leans towards democratic or participative style. While I establish the parameters for the work and have the ultimate decision-making authority, I also actively involve my team members in the process. This approach fosters trust between myself and my team as they feel engaged and valued. However, in high-pressure situations that require fast decision-making, this style may slow down the process. Additionally, if I am uncomfortable with disagreement or conflict, I may struggle with the potential responses to the consultation process.
+
+## Can u tell me a little about the game's you've created and the challenges u faced and over come?
+
+I have experience working on two games using Unity and C#. The first game was a 3D top-down destruction game where the player controls a bull to smash the environment for points. The main challenge was adapting to programming with a team and compromising ideas during meetings. However, it was a fulfilling experience and I look forward to working in a team environment again. For the second game, an Android match 3-style game, time management was the main constraint. Despite having only 48 non-consecutive hours to work on it, I was able to create a fully-featured game by balancing my goals with what was achievable.
+
+## Describe your most challenging project
+
+For me, the most challenging project so far was developing a match 3 style game for Android using Unity. The game's large scope posed several difficulties, especially when it came to incorporating tilemaps and designing for various mobile device dimensions. However, I was able to overcome these obstacles by implementing responsive design techniques and leveraging Unity's powerful features. In the end, the project turned out to be a great success and helped me grow as a developer.
+
+## Do you have any questions for me?
+
+Can you tell me more about the day-to-day responsibilities of this position?
+How does your company support the growth and development of employees?
+How does your company approach collaboration and teamwork among developers?
+What types of mentorship and guidance will be available to me during my internship?
+How does your company stay current with emerging trends and technologies in the field?
+Can you describe the company culture and work environment?
+What are the next steps in the hiring process, and when can I expect to hear back from you?
+
+## Tell me about a time you created a goal and achieved it
+
+Recently, I challenged myself to build a game within 48 non-consecutive hours as a warm-up for game jams. I set a goal to manage my time effectively and was able to produce a fairly complete product within the given timeframe. Through hard work and perseverance, I successfully achieved my goal.
+
+## Tell me about a time you disagreed with someone
+
+At my current job, I once disagreed with some coworkers about the way we stack empty crates in the corner of our workspace. While some of my colleagues preferred to stack the crates close to themselves, I believed that it was more efficient to stack them against the wall. To address this disagreement, I calmly moved the crates to the desired location and explained my reasoning behind it. This approach helped me to reach a compromise with my coworkers and ultimately led to a more efficient and safe work environment.
+
+## Tell me about a time you had to handle pressure
+
+At my current job, we often have audits from upper management which can put a lot of pressure on the team to perform well. While some people may feel stressed, I actually find pressure somewhat exciting as it motivates me to work even harder towards our common goal. During these times, I remain focused on the task at hand and work collaboratively with my colleagues to ensure that we are all contributing to the best of our abilities. By embracing the pressure, I am able to maintain a positive attitude and contribute to a successful outcome.
+
+## Tell me about a time you had to learn something quickly
+
+I believe that being able to learn quickly is a valuable skill in the tech industry, so I actively work on improving it. For instance, during the Github game jam, I had to develop a game using Unity within a month, despite never having used the system before. In order to quickly learn the system, I focused on online tutorials and forums to get up to speed on the basics. Additionally, I leveraged Unity's extensive documentation and community resources to help me overcome specific obstacles.
+
+## What do you want to get out of this internship?
+
+My main goal for this internship is to gain valuable experience and insights into working on a team that develops commercial code. While I have a lot of experience programming for my personal needs, I am eager to learn how to meet the needs of clients and contribute to meaningful projects. I am ready to step out of my comfort zone and adapt my skills to become a confident developer who can make a positive impact on the team and the company.
+
+## What is your greatest strength?
+
+My greatest strength is my ability to learn quickly. I have a knack for quickly picking up things that others sometimes struggle with. While I may not master everything I learn, I have acquired a diverse set of skills over the years. I am confident in my ability to use new technologies to create something meaningful in a short amount of time, often within a week.
+
+## What is your greatest weakness?
+
+One area that I am actively working on improving is my ability to give tough feedback. As someone who values positive relationships with colleagues, I sometimes struggle with providing constructive criticism that could be perceived as negative. However, I understand the importance of open communication and providing feedback that helps individuals grow and the team succeed. I have been working on techniques to give feedback in a more constructive way, and I am committed to continuously improving in this area.
+
+## What would your coworkers say about you?
+
+If you were to ask my co-workers about me, they would likely say that I am a hardworking and reliable team member. Having worked in the food retail industry for several years now, I understand the importance of dependability, and I pride myself on never having been late to a shift. In addition, I have a reputation among my colleagues for being the go-to person when someone needs a shift covered or another department requires assistance. I have cross-trained in several areas of the store, and I'm always happy to lend a hand wherever it's needed. Overall, I believe my co-workers would describe me as someone they can count on to get the job done.
+
+## Why do you want to join us?
+
+I am excited to join your team because I have heard great things about the company from my friends, who are currently working there. In particular, I respect the abilities of Steven and I believe that I can learn a lot from him. Additionally, I am impressed with your focus on producing high-quality, innovative software using a modern tech stack. I see this internship as an opportunity to gain valuable experience and contribute to the success of the company.
+
+## Why should we hire you?
+
+I can bring a solid foundation of skills and knowledge to the team, as well as a strong motivation to learn and grow within the role. I'm dedicated to continuous development and professional growth, and am a team player who is committed to the success of the company
+`;
+
+export { textToMemorize };
